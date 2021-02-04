f3cb14248075fc6a00a87e76dd79897a41dcff75
morpheusthewhite <zffromGerace@hotmail.it>
2021-02-04 18:49:25 +0100

    test(collectors): add twitter tests and correct reddit ones

--- tests/test_collectors.py
+++ tests/test_collectors.py
@@ -1,9 +1,11 @@
-import os
 import pytest
+import treelib
 
 
 from polarmine.collectors.reddit_collector import RedditCollector
 from polarmine.collectors.twitter_collector import TwitterCollector
+from polarmine.comment import Comment
+from polarmine.content import Content
 
 
 reddit_collector = RedditCollector()
@@ -12,38 +14,77 @@ twitter_collector = TwitterCollector()
 
 def test_reddit_collect_simple():
     # simple check on single content
-    contents = list(reddit_collector.collect(1))
+    contents = list(reddit_collector.collect(1, limit=10))
     assert len(contents) == 1
+
+    content = contents[0]
+    assert isinstance(content, treelib.Tree)
 
 
 def test_reddit_collect_more():
     # try to collect more than 1 content
-    contents = list(reddit_collector.collect(2))
-    assert len(contents) == 2
+    threads = list(reddit_collector.collect(2, limit=10))
+    assert len(threads) == 2
+
+    for thread in threads:
+        assert isinstance(thread, treelib.Tree)
+
+        root_id = thread.root
+        root = thread[root_id]
+        assert isinstance(root.data, Content)
+
+        children = thread.children(root_id)
+
+        if len(children) > 0:
+            assert isinstance(children[0].data, Comment)
 
 
 def test_reddit_collect_page():
     # try to collect from page
-    contents = list(reddit_collector.collect(2, page="programming"))
+    contents = list(reddit_collector.collect(2, page="programming",
+                                             limit=10))
     assert len(contents) == 2
 
 
 def test_reddit_collect_keyword():
     # try to collect from keyword
-    contents = list(reddit_collector.collect(2, keyword="obama"))
+    contents = list(reddit_collector.collect(2, keyword="obama",
+                                             limit=10))
     assert len(contents) == 2
-#
-#  def test_twitter_init_wrong_env():
-#      # instantiate a TwitterCollector with wrong environment
-#      env_var = "TWITTER_CONSUMER_KEY"
-#      env_value = os.getenv(env_var)
-#
-#
-#      # remove the variable and later restore it
-#      os.unsetenv(env_var)
-#      print(os.getenv(env_var))
-#
-#      with pytest.raises(Exception):
-#          TwitterCollector()
-#
-#      os.putenv(env_var, env_value)
+
+
+def test_twitter_collect_simple():
+    # simple check on single content
+    contents = list(twitter_collector.collect(1, keyword="obama",
+                                              limit=10))
+    assert len(contents) == 1
+
+    content = contents[0]
+    assert isinstance(content, treelib.Tree)
+
+
+def test_twitter_collect_more():
+    # try to collect more than 1 content
+    threads = list(twitter_collector.collect(2, keyword="obama",
+                                              limit=10))
+    assert len(threads) == 2
+
+    for thread in threads:
+        assert isinstance(thread, treelib.Tree)
+
+        root_id = thread.root
+        root = thread[root_id]
+        assert isinstance(root.data, Content)
+
+        children = thread.children(root_id)
+        if len(children) > 0:
+            assert isinstance(children[0].data, Comment)
+
+
+def test_twitter_collect_page():
+    # try to collect from page
+    contents = list(twitter_collector.collect(2, page="Cristiano",
+                                              limit=10))
+    assert len(contents) == 2
+
+
