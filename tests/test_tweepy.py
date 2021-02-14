a2cd3c965c33a230d0afce91488204b31b5efff0
morpheusthewhite <zffromGerace@hotmail.it>
2021-02-14 12:29:59 +0100

    test(tweepy): correct tests on id type

--- tests/test_tweepy.py
+++ tests/test_tweepy.py
@@ -1,4 +1,4 @@
-from polarmine.tweepy.models_v2 import SearchResultsv2, Statusv2
+from polarmine.tweepy.models_v2 import SearchResultsv2
 from polarmine.collectors.twitter_collector import TwitterCollector
 
 
@@ -27,19 +27,18 @@ def test_long_search():
     assert next_token is not None
     assert isinstance(search_result, SearchResultsv2)
     assert len(search_result) > 0
-    assert isinstance(search_result[0].id, str)
-    # assert search_result[0].text is None
+    assert isinstance(search_result[0].id, int)
 
 
 def test_complete_search():
     # retrieve a status and the full conversation related to it
     author = "vonderleyen"
-    thread = list(
-        twitter_collector.collect(1, page=author, limit=1, cross=False)
-    )[0]
-    root_status = thread.nodes[thread.root]
+    statuses = list(
+        twitter_collector.__find_statuses__(1, keyword=None, page=author)
+    )
+    root_status = statuses[0]
 
-    query = f"conversation_id:{root_status.identifier} to:{author}"
+    query = f"conversation_id:{root_status.id} to:{author}"
     search_result, next_token = twitter_collector.twitter.searchv2(query=query)
 
     while next_token is not None:
@@ -51,22 +50,23 @@ def test_complete_search():
 
         # first status in the current page of results
         status_0 = search_result[0]
-        assert isinstance(status_0.id, str)
+        assert isinstance(status_0.id, int)
         assert status_0.referenced_tweets is not None
 
 
 def test_get_ids():
     # retrieve a status and the full conversation related to it
     author = "vonderleyen"
-    thread = list(
-        twitter_collector.collect(1, page=author, limit=1, cross=False)
-    )[0]
-    root_status = thread.nodes[thread.root]
+    statuses = list(
+        twitter_collector.__find_statuses__(1, keyword=None, page=author)
+    )
+    root_status = statuses[0]
 
-    replies_dict = twitter_collector.twitter.get_replies_ids(
-        root_status.identifier, author)
+    replies_dict = twitter_collector.twitter.get_replies_id(
+        root_status.id, author
+    )
 
     for k, v in replies_dict.items():
-        assert isinstance(k, str)
+        assert isinstance(k, int)
         assert isinstance(v, list)
-        assert isinstance(v[0], str)
+        assert isinstance(v[0], int)
