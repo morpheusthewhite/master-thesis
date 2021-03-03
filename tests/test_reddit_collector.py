9265ba5309f540ad5d640911534621248741adae
morpheusthewhite <zffromGerace@hotmail.it>
2021-03-03 16:51:49 +0100

    test(reddit): update tests after content-thread

--- tests/test_reddit_collector.py
+++ tests/test_reddit_collector.py
@@ -3,7 +3,7 @@ import treelib
 
 from polarmine.collectors.reddit_collector import RedditCollector
 from polarmine.comment import Comment
-from polarmine.content import Content
+from polarmine.thread import Thread
 
 
 reddit_collector = RedditCollector()
@@ -11,26 +11,26 @@ reddit_collector = RedditCollector()
 
 def test_reddit_collect_simple():
     # simple check on single content
-    contents = list(reddit_collector.collect(1, limit=10, cross=False))
-    assert len(contents) == 1
+    discussion_trees = list(reddit_collector.collect(1, limit=10, cross=False))
+    assert len(discussion_trees) == 1
 
-    content = contents[0]
+    content = discussion_trees[0]
     assert isinstance(content, treelib.Tree)
 
 
 def test_reddit_collect_more():
     # try to collect more than 1 content
-    threads = list(reddit_collector.collect(2, limit=10, cross=False))
-    assert len(threads) == 2
+    discussion_trees = list(reddit_collector.collect(2, limit=10, cross=False))
+    assert len(discussion_trees) == 2
 
-    for thread in threads:
-        assert isinstance(thread, treelib.Tree)
+    for discussion_tree in discussion_trees:
+        assert isinstance(discussion_tree, treelib.Tree)
 
-        root_id = thread.root
-        root = thread[root_id]
-        assert isinstance(root.data, Content)
+        root_id = discussion_tree.root
+        root = discussion_tree[root_id]
+        assert isinstance(root.data, Thread)
 
-        children = thread.children(root_id)
+        children = discussion_tree.children(root_id)
 
         if len(children) > 0:
             assert isinstance(children[0].data, Comment)
@@ -38,22 +38,23 @@ def test_reddit_collect_more():
 
 def test_reddit_collect_page():
     # try to collect from page
-    contents = list(
+    discussion_trees = list(
         reddit_collector.collect(2, page="programming", limit=10, cross=False)
     )
-    assert len(contents) == 2
+    assert len(discussion_trees) == 2
 
 
 def test_reddit_collect_page_cross():
     # try to collect from page
-    contents = list(
+    discussion_trees = list(
         reddit_collector.collect(2, page="programming", limit=10, cross=True)
     )
-    assert len(contents) >= 2
+    assert len(discussion_trees) >= 2
 
 
 def test_reddit_collect_keyword():
     # try to collect from keyword
-    contents = list(reddit_collector.collect(2, keyword="obama", limit=10))
-    assert len(contents) >= 2
-
+    discussion_trees = list(
+        reddit_collector.collect(2, keyword="obama", limit=10)
+    )
+    assert len(discussion_trees) >= 2
