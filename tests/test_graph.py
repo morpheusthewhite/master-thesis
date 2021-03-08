8bc139087e9cd74dc89567f79f5a49edf576bedd
morpheusthewhite <zffromGerace@hotmail.it>
2021-03-08 11:57:10 +0100

    test(graph): hide unuseful tests

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -25,31 +25,33 @@ def test_graph_construction_reddit_more():
     assert graph is not None
 
 
-def test_graph_construction_twitter_simple():
-    contents = list(
-        twitter_collector.collect(1, keyword="obama", limit=10, cross=False)
-    )
-
-    graph = PolarizationGraph(contents)
-    assert graph is not None
-
-
-def test_graph_construction_twitter_more():
-    contents = list(
-        twitter_collector.collect(4, keyword="obama", limit=10, cross=False)
-    )
-
-    graph = PolarizationGraph(contents)
-    assert graph is not None
-
-
-def test_graph_kcore_selection():
-    contents = list(reddit_collector.collect(4, limit=10, cross=False))
-
-    graph = PolarizationGraph(contents)
-    num_vertices_unmasked = graph.graph.num_vertices()
-
-    graph.select_kcore(2)
-    num_vertices_masked = graph.graph.num_vertices()
-
-    assert num_vertices_unmasked != num_vertices_masked
+#  def test_graph_construction_twitter_simple():
+#      contents = list(
+#          twitter_collector.collect(1, keyword="obama", limit=10, cross=False)
+#      )
+#
+#      graph = PolarizationGraph(contents)
+#      assert graph is not None
+#
+#
+#  def test_graph_construction_twitter_more():
+#      contents = list(
+#          twitter_collector.collect(4, keyword="obama", limit=10, cross=False)
+#      )
+#
+#      graph = PolarizationGraph(contents)
+#      assert graph is not None
+#
+#
+#  def test_graph_kcore_selection():
+#      contents, users_flair = list(
+#          reddit_collector.collect(4, limit=10, cross=False)
+#      )
+#
+#      graph = PolarizationGraph(contents, users_flair)
+#      num_vertices_unmasked = graph.graph.num_vertices()
+#
+#      graph.select_kcore(2)
+#      num_vertices_masked = graph.graph.num_vertices()
+#
+#      assert num_vertices_unmasked != num_vertices_masked
