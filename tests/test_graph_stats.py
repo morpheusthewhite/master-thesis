3c1d193d999c59de44d503cd755654e844dd8dd6
morpheusthewhite <zffromGerace@hotmail.it>
2021-03-08 18:16:39 +0100

    revert: "test(graph): add social balance tests"
    This reverts commit 8d7dbac8f08f186315172552e946b36683efd9af.

--- tests/test_graph_stats.py
+++ tests/test_graph_stats.py
@@ -277,32 +277,3 @@ def test_graph_content_thread_neg_fraction_kcore():
 
         for neg_fraction in threads_dict.values():
             assert neg_fraction >= 0 and neg_fraction <= 1
-
-
-def test_graph_social_balance():
-    graph = PolarizationGraph.from_file(GRAPH_PATH)
-    assert graph is not None
-
-    frustration = graph.social_balance()
-    assert frustration >= 0
-
-
-def test_graph_social_balance_kcore():
-    graph = PolarizationGraph.from_file(GRAPH_PATH)
-    assert graph is not None
-
-    graph.select_kcore(2)
-
-    frustration = graph.social_balance()
-    assert frustration >= 0
-
-
-def test_graph_std_dev_dict():
-    graph = PolarizationGraph.from_file(GRAPH_PATH)
-    assert graph is not None
-
-    content_std_dev_dict = graph.content_std_dev_dict()
-
-    # 0 keys expected since cross is disabled and so there will be just one
-    # thread
-    assert len(content_std_dev_dict.keys()) == 0
