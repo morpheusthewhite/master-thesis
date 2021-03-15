b795ad773cc94a35d5fe60a372b63aba72995133
morpheusthewhite <zffromGerace@hotmail.it>
2021-03-15 09:41:05 +0100

    test(graph): add follow_graph tests

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,46 +1,62 @@
 from polarmine.collectors.reddit_collector import RedditCollector
 from polarmine.collectors.twitter_collector import TwitterCollector
 from polarmine.graph import PolarizationGraph
+from polarmine.follow_graph import FollowGraph
 
 
 reddit_collector = RedditCollector()
 twitter_collector = TwitterCollector()
 
 
+def test_follow_graph():
+    follow_dict = {1: [0, 2, 3], 2: [0, 3], 3: []}
+
+    follow_graph = FollowGraph(follow_dict)
+    assert follow_graph is not None
+
+    communities = list(follow_graph.communities())
+    assert len(communities) == 3
+
+
 def test_graph_construction_reddit_simple():
-    contents = list(reddit_collector.collect(1, limit=10, cross=False))
+    contents, follow_dict = reddit_collector.collect(1, limit=10, cross=False)
+    contents = list(contents)
 
     graph = PolarizationGraph(contents)
     assert graph is not None
 
 
 def test_graph_construction_reddit_more():
-    contents = list(reddit_collector.collect(4, limit=10))
+    contents, follow_dict = reddit_collector.collect(4, limit=10)
+    contents = list(contents)
 
     graph = PolarizationGraph(contents)
     assert graph is not None
 
 
 def test_graph_construction_twitter_simple():
-    contents = list(
-        twitter_collector.collect(1, keyword="obama", limit=10, cross=False)
+    contents, follow_dict = twitter_collector.collect(
+        1, keyword="obama", limit=10, cross=False
     )
+    contents = list(contents)
 
     graph = PolarizationGraph(contents)
     assert graph is not None
 
 
 def test_graph_construction_twitter_more():
-    contents = list(
-        twitter_collector.collect(4, keyword="obama", limit=10, cross=False)
+    contents, follow_dict = twitter_collector.collect(
+        4, keyword="obama", limit=10, cross=False
     )
+    contents = list(contents)
 
     graph = PolarizationGraph(contents)
     assert graph is not None
 
 
 def test_graph_kcore_selection():
-    contents = list(reddit_collector.collect(4, limit=10, cross=False))
+    contents, follow_dict = reddit_collector.collect(4, limit=10, cross=False)
+    contents = list(contents)
 
     graph = PolarizationGraph(contents)
     num_vertices_unmasked = graph.graph.num_vertices()
