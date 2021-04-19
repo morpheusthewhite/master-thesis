df8d27c8988fd9f870c9517e977c8f14c20bee8f
morpheusthewhite <zffromGerace@hotmail.it>
2021-04-19 22:44:01 +0200

    test(graph): add model parameters and save graph drawing

--- tests/test_synthetic.py
+++ tests/test_synthetic.py
@@ -1,12 +1,18 @@
 import time
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import graph_tool.spectral as gt
 
 from polarmine.graph import PolarizationGraph
 
+OUTDIR = "out"
+
 
 def test_synthetic():
+    if not os.path.exists(OUTDIR):
+        os.mkdir(OUTDIR)
+
     n_nodes_list = []
     n_threads_list = []
     omega_positive_list = []
@@ -35,7 +41,7 @@ def test_synthetic():
 
     # this second graph is much simpler. It has 40 vertices and 2
     # different echo chambers, one for each community
-    n_nodes = [30, 30]
+    n_nodes = [40, 40]
     omega_positive = [
         [0.8, 0.1],
         [0.1, 0.8],
@@ -46,11 +52,11 @@ def test_synthetic():
     ]
 
     n_nodes_list.append(n_nodes)
-    n_threads_list.append(2)
-    omega_positive_list.append(np.array(omega_positive) / 2)
-    omega_negative_list.append(np.array(omega_negative) / 2)
+    n_threads_list.append(5)
+    omega_positive_list.append(np.array(omega_positive) / 8)
+    omega_negative_list.append(np.array(omega_negative) / 8)
 
-    n_nodes = [30, 30]
+    n_nodes = [40, 40]
     omega_positive = [
         [0.45, 0.45],
         [0.45, 0.45],
@@ -61,9 +67,27 @@ def test_synthetic():
     ]
 
     n_nodes_list.append(n_nodes)
-    n_threads_list.append(2)
-    omega_positive_list.append(np.array(omega_positive) / 2)
-    omega_negative_list.append(np.array(omega_negative) / 2)
+    n_threads_list.append(5)
+    omega_positive_list.append(np.array(omega_positive) / 8)
+    omega_negative_list.append(np.array(omega_negative) / 8)
+
+    n_nodes = [40, 40]
+    omega_positive = [
+        [0.20, 0.20],
+        [0.20, 0.20],
+    ]
+    omega_negative = [
+        [0.60, 0.60],
+        [0.60, 0.60],
+    ]
+
+    n_nodes_list.append(n_nodes)
+    n_threads_list.append(5)
+    omega_positive_list.append(np.array(omega_positive) / 8)
+    omega_negative_list.append(np.array(omega_negative) / 8)
+
+    # keep track of iteration
+    i = 0
 
     for n_nodes, n_threads, omega_positive, omega_negative in zip(
         n_nodes_list, n_threads_list, omega_positive_list, omega_negative_list
@@ -87,7 +111,8 @@ def test_synthetic():
         for i, n_group_nodes in enumerate(n_nodes):
             communities += [i] * n_group_nodes
 
-        graph.draw(communities=communities)
+        outfile = os.path.join(OUTDIR, "graph" + str(i) + ".pdf")
+        graph.draw(output=outfile, communities=communities)
 
         start = time.time()
         score, _, _, _ = graph.score_mip(0.2)
@@ -99,7 +124,7 @@ def test_synthetic():
         print(f"Time: {end - start}")
         print("-" * 30)
 
-    assert False
+        i += 1
 
 
 test_synthetic()
