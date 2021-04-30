8ec0b9f5e419d30e3d3bd62d2fc0899e198f3cd5
morpheusthewhite <zffromGerace@hotmail.it>
2021-04-30 17:18:00 +0200

    test(densest): add find_bff_m test

--- tests/test_densest.py
+++ tests/test_densest.py
@@ -129,3 +129,36 @@ def test_dcs_am_score():
 
     score = densest.dcs_am_from_vertices(graph)
     assert score == 3
+
+
+def test_find_bff_m():
+    graph = gt.Graph()
+    contents_property = graph.new_edge_property("string")
+    graph.ep["content"] = contents_property
+
+    vertices = list(graph.add_vertex(5))
+
+    edge = graph.add_edge(vertices[1], vertices[0])
+    contents_property[edge] = "a"
+
+    edge = graph.add_edge(vertices[2], vertices[1])
+    contents_property[edge] = "a"
+
+    edge = graph.add_edge(vertices[3], vertices[4])
+    contents_property[edge] = "a"
+
+    edge = graph.add_edge(vertices[3], vertices[0])
+    contents_property[edge] = "b"
+
+    edge = graph.add_edge(vertices[1], vertices[2])
+    contents_property[edge] = "b"
+
+    edge = graph.add_edge(vertices[4], vertices[2])
+    contents_property[edge] = "b"
+
+    edge = graph.add_edge(vertices[2], vertices[1])
+    contents_property[edge] = "c"
+
+    score, vertices = densest.dcs_am_exact(graph)
+    assert score >= 2
+    assert 1 in vertices and 2 in vertices
