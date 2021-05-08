cb833d4747e6e81ac1fde495c6326eee931752ac
morpheusthewhite <zffromGerace@hotmail.it>
2021-05-08 11:57:11 +0200

    test(results): add option to save results

--- tests/test_pickled_results.py
+++ tests/test_pickled_results.py
@@ -1,6 +1,7 @@
 import pickle
 import argparse
 import sys
+import os
 
 from polarmine.graph import PolarizationGraph
 
@@ -32,19 +33,25 @@ parser.add_argument(
     dest="score",
     help=f"the result of which score to analyze. Possible values: {MIP_PARAM}, {ROUNDING_PARAM}, {MIP_DENSEST_PARAM}",
 )
+#  parser.add_argument(
+#      "--stats",
+#      "-st",
+#      type=str,
+#      default=None,
+#      metavar="filename",
+#      dest="stats_filename",
+#      help="filename of stats pickle containing the computed stats of the graph",
+#  )
 parser.add_argument(
-    "--stats",
-    "-st",
+    "--save-path",
     type=str,
+    help="directory in which to save results. If not provided shows results",
     default=None,
-    metavar="filename",
-    dest="stats_filename",
-    help="filename of stats pickle",
+    metavar="directory",
+    dest="save_path",
 )
 parser.add_argument(
-    "graph_filename",
-    type=str,
-    help="filename of graph",
+    "graph_filename", type=str, help="filename of graph", default=None
 )
 parser.add_argument(
     "--alpha",
@@ -59,17 +66,28 @@ parser.add_argument(
 def analyze(
     graph_filename: str = None,
     scores_filename: str = None,
-    stats_filename: str = None,
+    #  stats_filename: str = None,
+    save_path: str = None,
     score: str = None,
     alpha: float = 0.4,
 ):
     graph = PolarizationGraph.from_file(graph_filename)
     graph.remove_self_loops()
 
-    if stats_filename is not None:
-        stats_file = open(stats_filename, "w")
+    #  if stats_filename is not None:
+    #      stats_file = open(stats_filename, 'r')
+    #  else:
+    #      stats_file = sys.stdout
+
+    if save_path is not None:
+        if not os.path.exists(save_path):
+            os.mkdir(save_path)
+
+        outfile = os.path.join(save_path, "results_stats.txt")
+        graph_pdf_filename = os.path.join(save_path, "graph.pdf")
     else:
-        stats_file = sys.stdout
+        outfile = sys.stdout
+        graph_pdf_filename = None
 
     if alpha == -1:
         alpha = graph.alpha_median()
@@ -87,19 +105,28 @@ def analyze(
             score_key = MIP_DENSEST_KEY
         else:
             return
+        score_key = f"{score_key}_{alpha}"
 
         users = scores[score_key][1]
         graph.select_echo_chamber(alpha, users)
 
         print(
+            f"Number of vertices in the results: {graph.num_vertices()}",
+            file=outfile,
+        )
+        print(
+            f"Number of edges in the results: {graph.num_edges()}",
+            file=outfile,
+        )
+        print(
             f"Number of components in the resulting graph: {graph.num_components()}",
-            file=stats_file,
+            file=outfile,
         )
 
-        graph.draw(output=graph_filename)
+        graph.draw(output=graph_pdf_filename)
 
-        if stats_filename is not None:
-            stats_file.close()
+        if save_path is not None:
+            outfile.close()
 
 
 if __name__ == "__main__":
@@ -108,7 +135,7 @@ if __name__ == "__main__":
     analyze(
         args.graph_filename,
         args.scores_filename,
-        args.stats_filename,
+        args.save_path,
         args.score,
         args.alpha,
     )
