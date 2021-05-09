b9cd865c101608b8eb95e165e7b7157631e8c510
morpheusthewhite <zffromGerace@hotmail.it>
2021-05-09 18:19:45 +0200

    fix(graph): correct conflict

--- polarmine/graph.py
+++ polarmine/graph.py
@@ -1923,13 +1923,7 @@ class PolarizationGraph:
             current_edge_filter.a = np.logical_and(
                 current_edge_filter.a, np.logical_not(induced_edges_property.a)
             )
-<<<<<<< HEAD
-            # we invert the filter since the edges which are set to 1 are the
-            # ones to be ignored
-            self.graph.set_edge_filter(current_edge_filter, True)
-=======
             self.graph.set_edge_filter(current_edge_filter)
->>>>>>> 9cb850b (fix(graph): correct filtering in clustering routine)
 
             # compute jaccard coefficient for the current classification
             subgraph_vertices_assignment = vertices_assignment[vertices]
